4640a9156c7c090aa5fd0ca8663f41e9a5cf1943
Eric Dingle <eric.dingle@gmail.com>
2020-10-30 10:43:08 -0400

    Migrate analytics to latest tag snippet.

--- components/google-analytics.js
+++ components/google-analytics.js
@@ -1,6 +1,9 @@
 import {customElement} from '@polymer/decorators';
 import {PolymerElement, html} from '@polymer/polymer';
 
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+
 @customElement('google-analytics')
 class GoogleAnalytics extends PolymerElement {
   static get properties() { return {
@@ -11,17 +14,18 @@ class GoogleAnalytics extends PolymerElement {
   }}
 
   connectedCallback() {
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=UA-83209989-1';
+    document.body.appendChild(script);
 
-    ga('create', 'UA-83209989-1', 'auto');
+    gtag('js', new Date());
+    gtag('config', 'UA-83209989-1', {send_page_view: false});
   }
 
   pathChanged(path) {
     if (path) {
-      ga('send', 'pageview', path);
+      gtag('event', 'page_view', {page_path: path});
     }
   }
 }
